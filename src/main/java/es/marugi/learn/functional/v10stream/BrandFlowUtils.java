f7132ea2834c0b82fcf132108a6aa8645a805271
Mario Rubio <marugi@gmail.com>
2022-11-13 21:57:48 +0100

    fix: v10 stream

---
+++ src/main/java/es/marugi/learn/functional/v10stream/BrandFlowUtils.java
@@ -0,0 +1,60 @@
+package es.marugi.learn.functional.v10stream;
+
+import es.marugi.learn.App;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.*;
+
+public class BrandFlowUtils {
+
+    public static List<String> upperText(List<String> brandList){
+        List<String> result = new ArrayList<>();
+        for(String brand :  brandList){
+            result.add(brand.toUpperCase(Locale.ROOT));
+        }
+        return result;
+    }
+
+    public static boolean lengthBrandPredicate(Object brand){
+        return ((String)brand).length()>3;
+    }
+
+    public static int lengthComparator(Object value1, Object value2){
+        return ((String)value1).length()-((String)value2).length();
+    }
+
+    public static List<String> loadBrandsFromConfigFile(){
+
+        List<String> brandList = null;
+
+        try {
+
+            InputStream input = App.class.getClassLoader().getResourceAsStream("config.properties");
+
+            Properties prop = new Properties();
+
+            if (input == null) {
+                System.out.println("Sorry, unable to find config.properties");
+                throw new IOException();
+            }
+
+            // load a properties file from class path, inside static method
+            prop.load(input);
+
+            // get the property value and print it out
+            String[] brandArray = prop.getProperty("brand.name").split(",");
+
+            brandList = Arrays.asList(brandArray);
+
+            System.out.println(brandList);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return brandList;
+    }
+
+
+}
---
+++ src/main/java/es/marugi/learn/functional/v10stream/Test.java
@@ -0,0 +1,37 @@
+package es.marugi.learn.functional.v10stream;
+
+import es.marugi.learn.functional.v9optional.MyListFlow;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Test {
+
+    public static void main(String[] args){
+
+        System.out.println("Start v10stream.main()");
+        //https://www.w3schools.com/java/java_lambda.asp
+
+        List<String> brandList = new ArrayList<>(BrandFlowUtils.loadBrandsFromConfigFile());
+
+        // Native list stream
+        String maxLengthBrand = brandList.stream()
+                // Convert to uppercase
+                //.map(BrandFlowUtils::upperText)
+                // Filter list
+                .filter(BrandFlowUtils::lengthBrandPredicate)
+                // Sort brands by length
+                .sorted(BrandFlowUtils::lengthComparator)
+                // Prints the list
+                .peek(System.out::println)
+                // Get max length brand and returns an optional
+                .max(BrandFlowUtils::lengthComparator)
+                // If max gets an empty list returns an empty optional
+                .orElse("There is not items in the list");
+
+        System.out.println("Max Brand Length: " + maxLengthBrand );
+
+    }
+
+}
