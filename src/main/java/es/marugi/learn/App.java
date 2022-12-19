ce08df56d740f7259ba111ec709943e6dad0c06b
Mario Rubio <marugi@gmail.com>
2022-12-19 08:08:45 +0100

    fix: v10 stream

--- src/main/java/es/marugi/learn/App.java
+++ src/main/java/es/marugi/learn/App.java
@@ -1,6 +1,6 @@
 package es.marugi.learn;
 
-import es.marugi.learn.functional.v9optional.Test;
+import es.marugi.learn.functional.v10stream.Test;
 
 /**
  * Hello world!
--- src/main/java/es/marugi/learn/functional/v10stream/Test.java
+++ src/main/java/es/marugi/learn/functional/v10stream/Test.java
@@ -1,7 +1,11 @@
 package es.marugi.learn.functional.v10stream;
 
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 
 public class Test {
@@ -22,13 +26,57 @@ public class Test {
                 // Sort brands by length
                 .sorted(BrandFlowUtils::lengthComparator)
                 // Prints the list
-                .peek(System.out::println)
+                //.peek(System.out::println)
                 // Get max length brand and returns an optional
-                .max(BrandFlowUtils::lengthComparator)
+                //.max(BrandFlowUtils::lengthComparator)
+                .collect(Collectors.joining(";"));
                 // If max gets an empty list returns an empty optional
-                .orElse("There is not items in the list");
+                //.orElse("There is not items in the list");
 
-        System.out.println("Max Brand Length: " + maxLengthBrand );
+        System.out.println("CSV: " + maxLengthBrand );
+
+        //Stream generator
+
+        Random random = new Random();
+
+        //1 - Stream.of
+        System.out.printf("1 - Stream.of\n");
+        List<String> streamOf = Stream.of("Fulano", "Mengano","Paco", "Manolo", "Ana", "Joaquin")
+                .limit(5)
+                .collect(Collectors.toList());
+        System.out.println(streamOf);
+
+        //2 - Stream.generate
+        System.out.printf("2 - Stream.generate\n");
+        List<Integer> streamGenerate = Stream.generate(() -> random.nextInt(100))
+                .limit(25) // Limit stream to stop stream when we have 25 numbers
+                .collect(Collectors.toList()); // Convert to a List
+        System.out.println(streamGenerate);
+
+        //3a - Stream.iterate
+        System.out.printf("3a - Stream.iterate\n");
+        List<Integer> streamIterate = Stream.iterate(1, x -> x + x)
+                .limit(25) // Limit stream to stop stream when we have 25 numbers
+                .collect(Collectors.toList()); // Convert to a List
+        System.out.println(streamIterate);
+
+
+        //3b Fibonacci
+        System.out.printf("3b - Stream.iterate - Fibonacci\n");
+        List<Integer> fibo = Stream.iterate(new int[]{0, 1}, t -> new int[]{t[1], t[0] + t[1]})
+                .limit(25)
+                .map(t -> t[0])
+                .collect(Collectors.toList());
+        System.out.println(fibo);
+
+
+        //4 - Random.ints
+        System.out.printf("4 - Random.ints\n");
+        List<Integer> randomInts = random.ints(25, 1, 100)
+                .boxed()
+                .collect(Collectors.toList());
+        System.out.println(randomInts);
+
 
     }
 
